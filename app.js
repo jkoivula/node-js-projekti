f4b006ff80265e8a1db2d44b251cd9ca3e3a5950
Mindi Laihonen <mindi.laihonen@myy.haaga-helia.fi>
2017-01-03 19:45:23 +0200

    alkeellinen socket chat

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + 'client/index.html');
+  res.sendFile(__dirname + '/client/index.html');
 });
 
 app.use('/client', express.static(__dirname + '/client'));
 
-http.listen(3000);
+http.listen(3000, function(){
+  console.log('listening on port:3000');
+});
+
+io.on('connection', function(socket){
+  console.log('uusi socketyhteys');
+  socket.on('chat message', function(msg){
+      io.emit('chat message', msg);
+    });
+    socket.on('disconnect', function(){
+    console.log('socketyhteys katkaistu');
+  });
+});
