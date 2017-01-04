ab5ec8083e80096aaa6c9159724f48032bc250e3
Mindi Laihonen <mindi.laihonen@myy.haaga-helia.fi>
2017-01-04 20:19:28 +0200

    iideet lisätty käyttäjille

--- app.js
+++ app.js
@@ -13,9 +13,18 @@ server.listen(3000, function(){
   console.log('listening on port 3000');
 });
 
+var kayttajat = new Map();
+
 io.sockets.on('connection', function(socket){
+<<<<<<< HEAD
 
   console.log('uusi socketyhteys: '+socket.id);
+=======
+  socket.id = (Math.random() * 100) + 1;
+  var kayttaja = new UusiKayttaja(socket.id);
+  kayttajat.set(socket.id, kayttaja);
+  console.log(kayttajat.get(socket.id));
+>>>>>>> iideet lisätty käyttäjille
 
   socket.on('chat message', function(data){
       io.sockets.emit('chat message', data);
@@ -25,3 +34,20 @@ io.sockets.on('connection', function(socket){
     console.log('socketyhteys katkaistu');
   });
 });
+
+var UusiKayttaja = function(id) {
+  var kayttaja = {
+    id:id
+  }
+  return kayttaja;
+}
+// //"#"+((1<<24)*Math.random()|0).toString(16)
+// function getRandomColor(id) {
+//   var letters = id;
+//   var color = '#';
+//   for (var i = 0; i < 6; i++ ) {
+//       color += letters[Math.floor(Math.random().toString() * 16)];
+//   }
+//   console.log("heii "+ color);
+//   return color;
+// }
