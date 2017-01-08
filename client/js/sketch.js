6b1e7f0a5bd5f025a0f238bda637497702f456e0
Mindi Laihonen <mindi.laihonen@myy.haaga-helia.fi>
2017-01-08 20:40:32 +0200

    sketchissä käyttäjien nimet nyt värikkäitä ja muuta pientä

--- client/js/sketch.js
+++ client/js/sketch.js
@@ -1,13 +1,13 @@
 var socket;
 var puhekuplat = [];
-var scl = 35; // puhekuplien padding
+var scl = 30; // puhekuplien padding
 
 function setup() {
     var canvas = createCanvas(600, 500);
     canvas.parent("myCanvas");
     textAlign(LEFT, CENTER);
-    textSize(18);
-    textStyle(NORMAL);
+    textSize(14);
+    // HSB = hue, saturation, brightness
     colorMode(HSB, 100, 100, 100);
     socket.on('chat message', function(data) {
         var kupla = new Puhekupla(data.message, data.color, data.username);
@@ -17,29 +17,36 @@ function setup() {
 }
 
 function Puhekupla(msg, color, username) {
-    this.text = username + ": "+msg;
+    this.user = username + ":  ";
+    this.text = msg;
     this.color = color;
     this.x;
     this.y = height - scl;
-    this.w = textWidth(this.text)+scl;
+    this.uw = textWidth(this.user);
+    this.w = textWidth(this.text)+this.uw+scl;
     this.angle = 0;
-    this.r = random(scl, width - this.w);
+    this.r = random(scl, width - (this.w+scl));
 
     this.show = function() {
+        // näytä puhekupla
         var ang = radians(this.angle);
         this.x = this.r + (10 * cos(ang));
-        strokeWeight(0);
-        stroke(this.color, 30, 99);
-        fill(0);
-        text(this.text, this.x+scl/2, this.y, width-this.x, scl);
         noFill();
         strokeWeight(3);
+        stroke(this.color, 30, 100);
         rect(this.x, this.y, this.w, scl, scl);
+        // näytä käyttäjänimi ja viesti puhekuplan sisällä
+        textStyle(BOLD);
+        strokeWeight(0);
+        fill(this.color, 80, 80);
+        text(this.user, this.x+scl/2, this.y, width-this.x, scl);
+        textStyle(NORMAL);
+        fill(0);
+        text(this.text, this.x + scl/2 + this.uw, this.y, width-this.x, scl);
     }
 
     this.update = function() {
         this.y -= 0.5;
-        //this.x += random(-5,5);
         this.angle += 1.2;
     }
 }
