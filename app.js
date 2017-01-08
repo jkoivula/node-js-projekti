d97f984545c85f4a9c5ae40461ac8d2225751223
Mindi Laihonen <mindi.laihonen@myy.haaga-helia.fi>
2017-01-08 19:25:36 +0200

    funktio uuden käyttäjän vastaanottoon lisätty app.jsään

--- app.js
+++ app.js
@@ -18,17 +18,19 @@ var kayttajat = new Map();
 
 io.sockets.on('connection', function(socket){
 
-  socket.id = (Math.random() * 100) + 1;
-  var kayttaja = new UusiKayttaja(socket.id);
-  kayttajat.set(socket.id, kayttaja);
-  console.log(kayttajat.get(socket.id));
+  socket.on('new user', function(data) {
+    socket.id = (Math.random() * 100) + 1;
+    var kayttaja = new UusiKayttaja(socket.id, data.username);
+    kayttajat.set(socket.id, kayttaja);
+    io.sockets.emit('new user', kayttaja);
+  });
 
   socket.on('chat message', function(data){
     var x = {
       message: data.message,
-      color: kayttajat.get(socket.id).color
+      color: kayttajat.get(socket.id).color,
+      username: kayttajat.get(socket.id).username
     }
-    console.log(x.message);
     io.sockets.emit('chat message', x);
   });
 
@@ -37,27 +39,10 @@ io.sockets.on('connection', function(socket){
   });
 });
 
-// socket.on('sendMsgToServer',function(data){
-//         var playerName = ("" + socket.id).slice(2,7);
-//         for(var i in SOCKET_LIST){
-//             SOCKET_LIST[i].emit('addToChat',playerName + ': ' + data);
-//         }
-//     });
-
-
-var UusiKayttaja = function(id) {
+var UusiKayttaja = function(id, username) {
   var kayttaja = {
-    color:id
+    color:id,
+    username: username
   }
   return kayttaja;
 }
-// //"#"+((1<<24)*Math.random()|0).toString(16)
-// function getRandomColor(id) {
-//   var letters = id;
-//   var color = '#';
-//   for (var i = 0; i < 6; i++ ) {
-//       color += letters[Math.floor(Math.random().toString() * 16)];
-//   }
-//   console.log("heii "+ color);
-//   return color;
-// }
