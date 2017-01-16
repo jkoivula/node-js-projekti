14fa7bb783a8407cf51279f5e95a130bf0e16eb4
Mindi Laihonen <mindi.laihonen@myy.haaga-helia.fi>
2017-01-16 22:56:13 +0200

    canvas koko sivun kokoiseksi

--- client/js/sketch.js
+++ client/js/sketch.js
@@ -3,17 +3,18 @@ var puhekuplat = [];
 var scl = 30; // puhekuplien padding
 
 function setup() {
-    var canvas = createCanvas(600, 500);
+    var canvas = createCanvas(window.innerWidth, window.innerHeight);
     canvas.parent("myCanvas");
     textAlign(LEFT, CENTER);
     textSize(14);
-    // HSB = hue, saturation, brightness
     colorMode(HSB, 100, 100, 100);
+
     socket.on('chat message', function(data) {
         var kupla = new Puhekupla(data.message, data.color, data.username);
         console.log(kupla);
         puhekuplat.push(kupla);
     });
+
 }
 
 function Puhekupla(msg, color, username) {
@@ -21,28 +22,38 @@ function Puhekupla(msg, color, username) {
     this.text = msg;
     this.color = color;
     this.x;
-    this.y = height - scl;
+    this.y = 0;
     this.uw = textWidth(this.user);
-    this.w = textWidth(this.text)+this.uw+scl;
+    this.w = textWidth(this.text) + this.uw + scl;
     this.angle = 0;
-    this.r = random(scl, width - (this.w+scl));
+    var r = random(-250, 250);
+    this.r = constrain(r, -250, 250-this.w);
 
     this.show = function() {
-        // näytä puhekupla
+        push();
+        translate(width / 2, 500);
+
+        // arvotaan x-koordinaatti johon kupla ilmestyy ja lisätään aaltoliike
         var ang = radians(this.angle);
         this.x = this.r + (10 * cos(ang));
+
+        // piirretään puhekupla
         noFill();
         strokeWeight(3);
         stroke(this.color, 30, 100);
         rect(this.x, this.y, this.w, scl, scl);
-        // näytä käyttäjänimi ja viesti puhekuplan sisällä
+
+        // näytetään käyttäjänimi
         textStyle(BOLD);
         strokeWeight(0);
         fill(this.color, 80, 80);
-        text(this.user, this.x+scl/2, this.y, width-this.x, scl);
+        text(this.user, this.x + scl / 2, this.y, width - this.x, scl);
+
+        // näytetään käyttäjän viesti
         textStyle(NORMAL);
         fill(0);
-        text(this.text, this.x + scl/2 + this.uw, this.y, width-this.x, scl);
+        text(this.text, this.x + scl / 2 + this.uw, this.y, width - this.x, scl);
+        pop();
     }
 
     this.update = function() {
@@ -57,4 +68,9 @@ function draw() {
         puhekuplat[i].show();
         puhekuplat[i].update();
     };
+}
+
+// aseta canvasin koko uudelleen jos selainikkunan kokoa muutetaan
+function windowResized() {
+    resizeCanvas(window.innerWidth, window.innerHeight);
 }
